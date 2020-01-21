e3d9fc82ad997a399b62ac8c8ec0b11c1e659839
Miguel Bento <miguel.bento@eurail.com>
2020-01-21 22:32:22 +0100

    Rename script. Remove AWS. Improve Whitelist.
    This app previously used Amplify and aws services.
    With this patch any relation with aws disappears.

--- src/app.js
+++ src/app.js
@@ -9,14 +9,13 @@ const cors = require("cors");
 const whitelist = [
   "https://www.mr-monkey.net",
   "mr-monkey.net",
-  "https://mr-monkey.net/"
+  "https://mr-monkey.net/",
 ];
 
 const corsOptions = function(req, callback) {
   let corsOptions;
   if (whitelist.indexOf(req.header("Origin")) !== -1) {
     corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-    console.log(true);
   } else {
     corsOptions = { origin: false }; // disable CORS for this request
   }
